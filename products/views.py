8c211416257347bde5a6520875acb898fecaf67e
nilldiggonto <nilldiggonto@gmail.com>
2021-02-13 02:34:51 +0600

    Submit Form

--- products/views.py
+++ products/views.py
@@ -22,14 +22,32 @@ def chart_page(request):
     product_df = pd.DataFrame(qs1)
     purchase_df = pd.DataFrame(purchase.values())
     # print(product_df)
+    error_msg = None
+    df = ''
 
+    if purchase_df.shape[0]>0:
     #merging df
-    product_df['product_id'] = product_df['id']
-    df = pd.merge(purchase_df,product_df,on='product_id').drop(['id_y','created_y'],axis=1).rename({'id_x':'id','created_x':'date'},axis=1)
+        product_df['product_id'] = product_df['id']
+        df = pd.merge(purchase_df,product_df,on='product_id').drop(['id_y','created_y'],axis=1).rename({'id_x':'id','created_x':'date'},axis=1)
+
+
+        #form
+        if request.method == 'POST':
+            # print(request.POST)
+            chart_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+            # print(chart_type)
+            print(chart_type)
+            print(chart_type)
+            print(date_from,date_to)
+    else:
+        error_msg ='No Records Found'
     context = {
+        'error_msg':error_msg,
         'queryset':queryset,
         'product_df':product_df.to_html(),
         'purchase_df':purchase_df.to_html(),
-        'df':df.to_html(),
+        'df':df,
     }
     return render(request,template_name,context)
