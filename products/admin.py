88c77c4172fc5163e02d89eb42485da71fe7e257
nilldiggonto <nilldiggonto@gmail.com>
2021-02-13 00:55:59 +0600

    Purchase

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Purchase
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name','created']
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display= ['product','price','quantity','total_price','salesman']
+
+
--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
     name    = models.CharField(max_length=120)
@@ -7,3 +7,19 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Purchase(models.Model):
+    product     = models.ForeignKey(Product,on_delete=models.CASCADE)
+    price       = models.PositiveIntegerField()
+    quantity    = models.PositiveIntegerField()
+    total_price = models.PositiveBigIntegerField(blank=True,null=True)
+    salesman    = models.ForeignKey(User,on_delete=models.CASCADE)
+    created     = models.DateTimeField(auto_now_add=True)
+
+    def save(self,*args,**kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return f'{self.product}-{self.salesman.username}'
