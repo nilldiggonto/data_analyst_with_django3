bd99a46d4bfab33f5b4bbcff78c466ef64583b9f
nilldiggonto <nilldiggonto@gmail.com>
2021-02-13 00:45:01 +0600

    Model created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name    = models.CharField(max_length=120)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
