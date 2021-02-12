a5a5ea3b5f39b128476c6b49c05b670608c9c512
nilldiggonto <nilldiggonto@gmail.com>
2021-02-13 01:22:25 +0600

    Pandas DF

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,chart_page
 
 app_name = 'products'
 urlpatterns = [
     path('',home,name='home'),
+    path('chart/',chart_page,name='chart'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from .models import Product,Purchase
+
+import pandas as pd
+
 
 # Create your views here.
 def home(request):
     template_name = 'products/main.html'
     return render(request,template_name)
+
+def chart_page(request):
+    template_name = 'products/chart.html'
+    queryset = Product.objects.all()
+    qs1 = Product.objects.all().values()
+    qs2 = Product.objects.all().values_list()
+
+    purchase = Purchase.objects.all()
+    # print(qs1)
+    # print(qs2)
+
+    product_df = pd.DataFrame(qs1)
+    purchase_df = pd.DataFrame(purchase.values())
+    # print(product_df)
+    context = {
+        'queryset':queryset,
+        'product_df':product_df.to_html(),
+        'purchase_df':purchase_df.to_html(),
+    }
+    return render(request,template_name,context)
